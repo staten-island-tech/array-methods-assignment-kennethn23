ea62965ff6cb7da5002ce903a44f4d14900dd66f
kennethn23 <kennethng1337@gmail.com>
2023-10-05 17:55:56 -0400

    10/5/23 2

--- objects.js
+++ objects.js
@@ -1,18 +1,23 @@
-/* const Jett = {
-    gender: "Female",
-    release: "June 2, 2020",
-    role: "Deulist",
-    abilities: ["Drift", "Cloudburst", "Updraft", "Tailwind", "Blade Storm"],
+function agentNames (agent) {
+    console.log(agent.name);
 }
 
-const Reyna = {
-    gender: "Female",
-    release: "June 2, 2020",
-    role: "Deulist",
-    abilities: ["Leer", "Devour", "Dismiss", "Empress"],
-} */
+function agentStats (agent) {
+    console.log(agent);
+}
 
-const agents = [{name: "Jett", release: "June 2, 2020", role: "Deulist", abilities: ["Drift", "Cloudburst", "Updraft", "Tailwind", "Blade Storm"]},
-{name: "Reyna", release: "June 2, 2020", role: "Deulist", abilities: ["Leer", "Devour", "Dismiss", "Empress"]}]
+function roleCheck (agent) {
+    return agent.role === "Deulist";
+}
 
-agents.forEach((element) => console.log(element));
+const agents = [
+    {name: "Jett", release: "April 7, 2020", role: "Deulist", abilities: ["Drift", "Cloudburst", "Updraft", "Tailwind", "Blade Storm"]},
+    {name: "Neon", release: "January 11, 2022", role: "Deulist", abilities: ["Fast Lane", "Relay Bolt", "High Gear", "Overdrive"]},
+    {name: "Killjoy", release: "August 4, 2020", role: "Sentinel", abilities: ["Nanoswarm", "Alarmbot", "Turret", "Lockdown"]},
+    {name: "Harbor", release: "October 18, 2022", role: "Controller", abilities: ["Cascade", "Cove", "High Tide", "Reckoning"]},
+    {name: "KAY/O", release: "June 22, 2021", role: "Initiator", abilities: ["Frag/Ment", "Flash/Drive", "Zero/Point", "Null/CMD"]},
+]
+
+agents.forEach(agentNames);
+agents.forEach(agentStats);
+console.log(agents.filter(roleCheck));

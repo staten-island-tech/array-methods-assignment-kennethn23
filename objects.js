9a61a07336b3781f53f26f3e5d9662243714899d
kennethn23 <kennethng1337@gmail.com>
2023-10-05 09:23:53 -0400

    10/5/23

---
+++ objects.js
@@ -0,0 +1,18 @@
+/* const Jett = {
+    gender: "Female",
+    release: "June 2, 2020",
+    role: "Deulist",
+    abilities: ["Drift", "Cloudburst", "Updraft", "Tailwind", "Blade Storm"],
+}
+
+const Reyna = {
+    gender: "Female",
+    release: "June 2, 2020",
+    role: "Deulist",
+    abilities: ["Leer", "Devour", "Dismiss", "Empress"],
+} */
+
+const agents = [{name: "Jett", release: "June 2, 2020", role: "Deulist", abilities: ["Drift", "Cloudburst", "Updraft", "Tailwind", "Blade Storm"]},
+{name: "Reyna", release: "June 2, 2020", role: "Deulist", abilities: ["Leer", "Devour", "Dismiss", "Empress"]}]
+
+agents.forEach((element) => console.log(element));

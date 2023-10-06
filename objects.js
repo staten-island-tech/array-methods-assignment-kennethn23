1775b3f0927769b828a73e9202b0c5bb85701861
kennethn23 <kennethng1337@gmail.com>
2023-10-06 09:19:44 -0400

    10/6/23

--- objects.js
+++ objects.js
@@ -1,23 +1,36 @@
-function agentNames (agent) {
-    console.log(agent.name);
-}
-
-function agentStats (agent) {
-    console.log(agent);
-}
-
-function roleCheck (agent) {
-    return agent.role === "Deulist";
-}
-
 const agents = [
-    {name: "Jett", release: "April 7, 2020", role: "Deulist", abilities: ["Drift", "Cloudburst", "Updraft", "Tailwind", "Blade Storm"]},
-    {name: "Neon", release: "January 11, 2022", role: "Deulist", abilities: ["Fast Lane", "Relay Bolt", "High Gear", "Overdrive"]},
-    {name: "Killjoy", release: "August 4, 2020", role: "Sentinel", abilities: ["Nanoswarm", "Alarmbot", "Turret", "Lockdown"]},
-    {name: "Harbor", release: "October 18, 2022", role: "Controller", abilities: ["Cascade", "Cove", "High Tide", "Reckoning"]},
-    {name: "KAY/O", release: "June 22, 2021", role: "Initiator", abilities: ["Frag/Ment", "Flash/Drive", "Zero/Point", "Null/CMD"]},
+    {
+        name: "Jett", 
+        release: "April 7, 2020", 
+        role: "Deulist", 
+        abilities: ["Drift", "Cloudburst", "Updraft", "Tailwind", "Blade Storm"],
+    },
+    {
+        name: "Neon", 
+        release: "January 11, 2022", 
+        role: "Deulist", 
+        abilities: ["Fast Lane", "Relay Bolt", "High Gear", "Overdrive"],
+    },
+    {
+        name: "Killjoy", 
+        release: "August 4, 2020", 
+        role: "Sentinel", 
+        abilities: ["Nanoswarm", "Alarmbot", "Turret", "Lockdown"],
+    },
+    {
+        name: "Harbor", 
+        release: "October 18, 2022", 
+        role: "Controller", 
+        abilities: ["Cascade", "Cove", "High Tide", "Reckoning"],
+    },
+    {
+        name: "KAY/O", 
+        release: "June 22, 2021", 
+        role: "Initiator", 
+        abilities: ["Frag/Ment", "Flash/Drive", "Zero/Point", "Null/CMD"],
+    },
 ]
 
-agents.forEach(agentNames);
-agents.forEach(agentStats);
-console.log(agents.filter(roleCheck));
+agents.forEach((agent) => console.log(agent.name));
+agents.forEach((agent) => console.log(agent));
+console.log(agents.filter((agent) => agent.role === "Deulist"));
